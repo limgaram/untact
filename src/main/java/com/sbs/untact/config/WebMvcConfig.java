0f4e603428a95ccdee88ab027bf1fcd4a658b9c9
limgaram <garam990317@naver.com>
2021-02-27 20:30:05 +0900

    외부 로그인을 위해 회원테이블에 authKey칼럼 추가

--- src/main/java/com/sbs/untact/config/WebMvcConfig.java
+++ src/main/java/com/sbs/untact/config/WebMvcConfig.java
@@ -36,15 +36,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("resource/**");
 
 		// 어드민 필요
-		registry.addInterceptor(needAdminInterceptor).addPathPatterns("/adm/**")
-				.excludePathPatterns("/adm/member/login").excludePathPatterns("/usr/member/doLogin")
-				.excludePathPatterns("/usr/member/join").excludePathPatterns("/usr/member/doJoin")
-				.excludePathPatterns("/usr/article/list").excludePathPatterns("/usr/article/detail")
-				.excludePathPatterns("/usr/reply/list").excludePathPatterns("/usr/member/findLoginId")
-				.excludePathPatterns("/usr/member/doFindLoginId").excludePathPatterns("/usr/member/findLoginPw")
-				.excludePathPatterns("/usr/member/doFindLoginPw").excludePathPatterns("/usr/file/test*")
-				.excludePathPatterns("/usr/file/doTest*").excludePathPatterns("/test/**").excludePathPatterns("/error");
-
+		registry.addInterceptor(needAdminInterceptor)
+				.addPathPatterns("/adm/**")
+				.excludePathPatterns("/adm/member/login")
+				.excludePathPatterns("/adm/member/doLogin");
+		
 		// 로그인 필요
 		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/**").excludePathPatterns("/")
 				.excludePathPatterns("/adm/**").excludePathPatterns("/resource/**")
