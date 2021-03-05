b6aa9e50b8804e1aa85d0d341ef63534ca4dd6f9
limgaram <garam990317@naver.com>
2021-03-05 23:38:52 +0900

    memberControlle 버그 수정r

--- src/main/java/com/sbs/untact/controller/AdmMemberController.java
+++ src/main/java/com/sbs/untact/controller/AdmMemberController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ import com.sbs.untact.dto.ResultData;
 import com.sbs.untact.service.MemberService;
 import com.sbs.untact.util.Util;
 
+@Controller
 public class AdmMemberController {
 
 	@Autowired
